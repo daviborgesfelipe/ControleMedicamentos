c223e9427b8d26682a99310f6d9a703242947c2d
Davi Borges <davifelipe_300@hotmail.com>
2023-04-18 21:54:18 -0300

    feat: Inicia o projeto com arquitetura em tres camadas com separacao de responsabilidade.

---
+++ ControleMedicamentos.ConsoleApp/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,6 @@
+﻿namespace ControleMedicamentos.ConsoleApp.Compartilhado
+{
+    internal class EntidadeBase
+    {
+    }
+}
---
+++ ControleMedicamentos.ConsoleApp/ModuloMedicamentos/RepositorioMedicamento.cs
@@ -0,0 +1,8 @@
+﻿using ControleMedicamentos.ConsoleApp.Compartilhado;
+
+namespace ControleMedicamentos.ConsoleApp.ModuloMedicamentos
+{
+    internal class RepositorioMedicamento : RepositorioBase
+    {
+    }
+}
